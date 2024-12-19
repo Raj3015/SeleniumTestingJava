a4ad791226f997b7ef865e31dedecfc29ac0e203
Raj3015 <boss4aws@gmail.com>
2024-12-19 11:20:18 +0530

    Update AppTest.java

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -15,7 +15,7 @@ public class AppTest
 	@Test             
 	public void test1() {      
 		
-		WebDriverManager.chromedriver().setup();
+		WebDriverManager.chromedriver().clearDriverCache().setup();
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--no-sandbox");
 		options.addArguments("--disable-dev-shm-usage");
